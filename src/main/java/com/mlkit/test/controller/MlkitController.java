f44aefc73ab4526170937cd2aadfb08f18b509f1
박진영 <nazzang49@naver.com>
2020-09-20 23:25:30 +0900

    Basic Setting

--- src/main/java/com/mlkit/test/controller/MlkitController.java
+++ src/main/java/com/mlkit/test/controller/MlkitController.java
@@ -4,13 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * ML Kit test controller
+ */
 @Controller
 @Slf4j
 public class MlkitController {
 
+    /**
+     * 테스트 페이지
+     *
+     * @return
+     */
     @GetMapping("/test")
     public String test() {
         return "view/test";
     }
+
+
 
 }
---
+++ src/main/java/com/mlkit/test/service/MlkitService.java
@@ -0,0 +1,15 @@
+package com.mlkit.test.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * ML Kit test service
+ */
+@Service
+@Slf4j
+public class MlkitService {
+
+
+
+}
